9c776d08ed2a55bb70b4748cace54c9b7991c13c
dongxu <huang@pingcap.com>
2022-05-20 22:39:45 -0700

    add stat

--- pubsub/stat.go
+++ pubsub/stat.go
@@ -14,5 +14,50 @@
 
 package pubsub
 
+import (
+	"encoding/json"
+	"sync"
+)
+
 type Stat struct {
+	mu sync.RWMutex
+	m  map[string]interface{}
+}
+
+var sysStat *Stat
+
+func init() {
+	sysStat = new(Stat)
+}
+
+func Stat() *Stat {
+	return sysStat
+}
+
+func (s *Stat) Set(key string, val interface{}) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.m[key] = val
+}
+
+func (s *Stat) Inc(key string, val int) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if v, ok := s.m[key]; ok {
+		if v, ok := v.(int); ok {
+			s.m[key] = v + val
+		}
+	} else {
+		s.m[key] = val
+	}
+}
+
+func (s *Stat) JSON() (string, error) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	b, err := json.Marshal(s.m)
+	if err != nil {
+		return "", err
+	}
+	return string(b), nil
 }
